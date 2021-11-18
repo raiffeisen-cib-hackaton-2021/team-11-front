770981b3f9da452c18820e9ca6c29c356aa5ae46
popuguytheparrot <19804652+popuguytheparrot@users.noreply.github.com>
2021-11-18 12:25:16 +0300

    feat: add navbar links

--- src/features/NavBar/NavBar.jsx
+++ src/features/NavBar/NavBar.jsx
@@ -1,11 +1,25 @@
-import { Logotype, Whitespace } from "@fcc/ui";
+import { Logotype, Flex, Button, Link as LinkDS } from "@fcc/ui";
+import { Notification } from "@fcc/icons";
+import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
     <nav className="header">
-      <Whitespace p="s4">
-        <Logotype />
-      </Whitespace>
+      <Flex p="s4" justifyContent="space-between">
+        <Flex.Item>
+          <LinkDS as={Link} to="/">
+            <Logotype />
+          </LinkDS>
+        </Flex.Item>
+        <Flex.Item>
+          <LinkDS as={Link} to="/notifications">
+            <Button design="ghost">
+              <Notification />
+              Уведомления
+            </Button>
+          </LinkDS>
+        </Flex.Item>
+      </Flex>
       <hr />
     </nav>
   );
